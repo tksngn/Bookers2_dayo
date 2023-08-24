33e3e0734054bf592090c3857f947ef075a0bcd1
tksngn <138876045+tksngn@users.noreply.github.com>
2023-08-24 12:53:59 +0000

    [fix]varidates

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+end
